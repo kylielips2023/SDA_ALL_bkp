6bc544f0b6501ba4b1d8d91ba351c839fb614c46
GH0STAV0 <you@example.com>
2022-12-16 07:22:49 -0600

    docker with node

--- main_oct0pus/cnf_bvb.py
+++ main_oct0pus/cnf_bvb.py
@@ -69,6 +69,9 @@ def send_msg_dock(text):
 
 def extact_gc_profile():
 	print("extact")
+	remove_old_folder="rm -r "+g00g_acc+"*"
+	os.system(remove_old_folder)
+
 	comom="cp po/"+g00g_acc+".tar.gz ./"
 	os.system(comom)
 	name_archive_account=g00g_acc+'.tar.gz'
