e9a4404f4ae0933b4f5e442df621bd12a3e890f2
GH0STAV0 <you@example.com>
2022-12-30 08:46:32 +0100

    docker with node

--- site_5/teleg_untitled.py
+++ site_5/teleg_untitled.py
@@ -11,11 +11,10 @@ telegram_tokens_van="5842682178:AAHhcZ41vh4XpIXvVhZ0-RuN0KkCClVgg4g"
 chat_id_alerts_van_google = "-1001633899177"
 hostname_os=""
 
-# def send_msg(text):
+def send_msg(text):
 
-# 	msg_telegram="[ "+hostname_os +" ]"+text
-# 	token = telegram_tokens_van
-# 	chat_id = chat_id_alerts_van_google
-# 	url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + msg_telegram 
-# 	results = requests.get(url_req)
-
+	msg_telegram="[ "+hostname_os +" ]"+text
+	token = telegram_tokens_van
+	chat_id = chat_id_alerts_van_google
+	url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + msg_telegram 
+	results = requests.get(url_req)
--- vanish_bo/para_m.py
+++ vanish_bo/para_m.py
@@ -2,4 +2,6 @@
 # url="https://api-1-u0aw.onrender.com"
 # url="https://nd-my-01.onrender.com"
 # url="https://apiv5.onrender.com"
-url="https://apiv6.onrender.com"
+# url="https://apiv6.onrender.com"
+url="https://apiv7-koy01apiv7.koyeb.app"
+
