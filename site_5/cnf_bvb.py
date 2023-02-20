fb05022a565140b3c2e66723ea8dc1d4066c6523
GH0STAV0 <you@example.com>
2023-02-20 04:15:38 +0100

    last commit 19 jan

--- site_5/cnf_bvb.py
+++ site_5/cnf_bvb.py
@@ -26,7 +26,7 @@ def append_to_l0g(text_add):
 
 vpn_type="N"
 total_l0g=[]
-vversion="SITE_5 V 6.0 io/IMG = xm0uray-site_5 | chrome api ++v977"
+vversion="101_SITE_5 V 6.0 io/IMG = xm0uray-site_5 | chrome api ++v977"
 telegram_tokens_bot=["0","5036803152:AAGs0ES1OmEdy86MNJDp7mp19BB5IQhcVHU","5099462819:AAEndTxvXaSqBQ6E_EpiCN02a81ROGPMgr0","5001651751:AAGzzbUfJXWHZz-FKJyLSUxzg-JiRMO5v-Q","5041058138:AAFRher-cMwnRI476iW24tT6Kt8lvC0bmLc","5051743922:AAEOHJTRzv2WIxZ8bR-VrVYNA6io6qB1Ltw"]
 
 
