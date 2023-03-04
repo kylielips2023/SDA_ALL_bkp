36cc7a058a3343d886feaf02e318370c7e3c6f3d
GH0STAV0 <you@example.com>
2023-03-04 18:35:04 +0100

    last commit 19 jan

--- n_main_oct0pus_van/cnf_bvb.py
+++ n_main_oct0pus_van/cnf_bvb.py
@@ -22,7 +22,8 @@ path2 = Path(path_to_file2)
 if path2.is_file():
     print(f'The file {path_to_file2} exists')
 else:
-    print(f'The file {path_to_file} does not exist')
+    print(f'The file {path_to_file2} does not exist')
+    os.system('mkdir -p /root/EXTRAT && mkdir -p /root/EXTRAT/firefox-97.0.1/')
     os.system("wget -q https://ftp.mozilla.org/pub/firefox/releases/97.0.1/linux-x86_64/en-GB/firefox-97.0.1.tar.bz2 && tar -xf firefox-97.0.1.tar.bz2 -C /root/EXTRAT/firefox-97.0.1/")
 
 
